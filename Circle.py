34a4232a34f655eb3bdae7c8c0d65ae0a6d194c8
AnanGharra <73491844+AnanGharra@users.noreply.github.com>
2024-04-15 22:45:04 +0300

    Create Circle class

---
+++ Circle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import math
+
+class Circle:
+    "A class representing a circle."
+    
+    def __init__(self, radius):
+        "Initialize the circle with a radius."
+        self.radius = radius
+
+    def get_radius(self):
+        "Return the radius of the circle."
+        return self.radius
+
+    def set_radius(self, radius):
+        "Set the radius of the circle."
+        self.radius = radius
+
+    def area(self):
+        "Calculate and return the area of the circle."
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        "Calculate and return the perimeter of the circle."
+        return 2 * math.pi * self.radius
