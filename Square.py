f08ab85c42dafc8f8c402dca3280fa23ce009d7e
AnanGharra <73491844+AnanGharra@users.noreply.github.com>
2024-04-15 22:48:27 +0300

    Create Square class

---
+++ Square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+class Square:
+    "A class representing a square."
+
+    def __init__(self, side_length):
+        "Initialize the square with a side length."
+        self.side_length = side_length
+
+    def get_side_length(self):
+        "Return the side length of the square."
+        return self.side_length
+
+    def set_side_length(self, length):
+        "Set the side length of the square."
+        self.side_length = length
+
+    def area(self):
+        "Calculate and return the area of the square."
+        return self.side_length ** 2
+
+    def perimeter(self):
+        "Calculate and return the perimeter of the square."
+        return 4 * self.side_length
