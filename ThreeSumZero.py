10233abebff119eae7e07de0f2d00cf5b88e734d
AnanGharra <73491844+AnanGharra@users.noreply.github.com>
2024-04-16 00:32:40 +0300

    Create ThreeSumZero class

---
+++ ThreeSumZero.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+class ThreeSumZero:
+    "Class to find all unique triplets in the array which gives the sum of zero."
+    
+    def find_triplets(self, nums):
+        nums.sort()
+        result = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum_ = nums[i] + nums[left] + nums[right]
+                if sum_ == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif sum_ < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return result
+
